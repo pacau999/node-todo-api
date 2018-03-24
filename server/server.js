67082fdb19271808ccc1197f82ab8691f23cebb0
pacau999 <pacau999@yahoo.com.br>
2018-03-24 19:58:27 -0300

    =Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.listen(3000, () =>{
     console.log('Starded on port 3000');
 })
 
+module.exports ={app};
+
 // var newTodo = new Todo({text:'fomdaze ee'});
 
 // newTodo.save().then((doc)=>{
