b511440f76f8fc1c958de6a56b13c716745c6432
pacau999 <pacau999@yahoo.com.br>
2018-03-26 21:55:44 -0300

    =added /DELETE:id

--- server/server.js
+++ server/server.js
@@ -47,6 +47,22 @@ app.get('/todos/:id', (req, res)=> {
 
 });
 
+app.delete('/todos/:id',(req, res)=>{
+    var id = req.params.id;
+    if(!Types.ObjectId.isValid(id)){
+        res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(id).then((todo) => {
+     if(todo){
+         res.send(todo);
+     }
+         res.status(404).send();
+    },()=>{
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () =>{
     console.log('Starded on port: ',port);
 });
