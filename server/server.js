dee2160041e3a3fa73ad4498e31ab49154224b75
pacau999 <pacau999@yahoo.com.br>
2018-03-26 22:22:23 -0300

    =Added /PATCH:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo, Types} = require('./models/todo');
@@ -18,31 +20,31 @@ app.post('/todos',(req, res) =>{
     todo.save().then((doc)=>{
         res.send(doc);
     },(e)=>{
-        res.status(400).send(e.message);
+        return res.status(400).send(e.message);
     })
 });
 
 app.get('/todos', (req, res)=>{
     Todo.find().then((todos)=>{
         res.send({todos});
-    },(e)=>{
-        res.status(400).send(e);
+    }).catch((e)=>{
+        return  res.status(400).send(e);
     });
 });
 
 app.get('/todos/:id', (req, res)=> {
    var id = req.params.id;
    if(!Types.ObjectId.isValid(id)){
-       res.status(404).send();
+    return  res.status(404).send();
    }
    
    Todo.findById(id).then((todo) => {
-    if(todo){
-        res.send(todo);
+    if(!todo){
+        return  res.status(404).send();
     }
-        res.status(404).send();
-   },()=>{
-       res.status(400).send();
+    res.send(todo);
+   }).catch((e)=>{
+        return res.status(400).send();
    });
 
 });
@@ -50,22 +52,46 @@ app.get('/todos/:id', (req, res)=> {
 app.delete('/todos/:id',(req, res)=>{
     var id = req.params.id;
     if(!Types.ObjectId.isValid(id)){
-        res.status(404).send();
+        return res.status(404).send();
     }
     
     Todo.findByIdAndRemove(id).then((todo) => {
-     if(todo){
-         res.send(todo);
+     if(!todo){
+        return res.status(404).send();
      }
-         res.status(404).send();
-    },()=>{
-        res.status(400).send();
+     res.send(todo);
+    }).catch((e)=>{
+        return res.status(400).send();
     });
 });
 
+app.patch('/todos/:id',(req,res) =>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+    if(!Types.ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed - false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,{$set: body}, {new:true}).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((e)=>{
+
+    });
+});
 app.listen(port, () =>{
     console.log('Starded on port: ',port);
 });
+
+
 
 module.exports ={app};
 
